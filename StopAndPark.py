7d4304847f935c398f712f119638d9ba38ea4c0b
Pade88 <ovidiu.padeanu@gmail.com>
2019-05-17 18:41:31 +0300

    17.05 18:41

--- StopAndPark.py
+++ StopAndPark.py
@@ -1,28 +1,13 @@
+import numpy as np
 import cv2
-import numpy as np
-
-cap = cv2.VideoCapture('cameraG.avi')
-
-
-import numpy as np
-
-class Indicator:
-    STOP = 1
-    PARCARE = 2
-    Eroare = 3
-
 
 font = cv2.FONT_HERSHEY_COMPLEX
-def stopOrPark(frame,AMPARCAT=False):
-    #frame = cv2.resize(frame, None, fx=0.5, fy=0.6, interpolation=cv2.INTER_CUBIC)
-    frame = frame[0:350, 320:640]
-    counterBlue = 0
-    counterRed = 0
-    hsv = cv2.cvtColor(frame, cv2.COLOR_BGR2HSV)
 
-    lower_blue = np.array([90, 80, 50])
-    upper_blue = np.array([110, 255, 255])
-    maskBlue = cv2.inRange(hsv, lower_blue, upper_blue)
+def stopOrPark(frame):
+    #frame = cv2.resize(frame, None, fx=0.5, fy=0.6, interpolation=cv2.INTER_CUBIC)
+    global AmParcat
+    frame = frame[0:350, 320:640]
+    hsv = cv2.cvtColor(frame, cv2.COLOR_BGR2HSV)
 
     lower_red = np.array([0, 50, 50])
     upper_red = np.array([10, 255, 255])
@@ -32,27 +17,25 @@ def stopOrPark(frame,AMPARCAT=False):
     upper_red = np.array([180, 255, 255])
     mask2 = cv2.inRange(hsv, lower_red, upper_red)
 
+
     mask = mask1 + mask2
     kernel = np.ones((1, 1), np.uint8)
-    maskRed = cv2.erode(mask, kernel)
+    #maskRed = cv2.erode(mask, kernel)
 
     lower_white = np.array([0, 0, 0])
     upper_white = np.array([0, 0, 255])
     maskWhite = cv2.inRange(hsv, lower_white, upper_white)
 
+    contoursRed, none = cv2.findContours(mask, cv2.RETR_TREE, cv2.CHAIN_APPROX_SIMPLE)
+    contoursWhite, none = cv2.findContours(maskWhite, cv2.RETR_TREE, cv2.CHAIN_APPROX_SIMPLE)
 
-    kernelBlue = np.ones((1, 1), np.uint8)
-    maskBlue = cv2.erode(maskBlue, kernelBlue)
-    contoursRed = cv2.findContours(maskRed, cv2.RETR_TREE, cv2.CHAIN_APPROX_SIMPLE)
-
-    contoursWhite = cv2.findContours(maskWhite, cv2.RETR_TREE, cv2.CHAIN_APPROX_SIMPLE)
-
+    counterRed = 0
     for cnt in contoursRed:
         haveWhite = False
         area = cv2.contourArea(cnt)
         approx = cv2.approxPolyDP(cnt, 0.015 * cv2.arcLength(cnt, True), True)  # aproximeaza suprafata
 
-        if  area > 400:
+        if  area > 200:
             counterRed = counterRed + 1
             for cnt2 in contoursWhite:
                 approxWhite = cv2.approxPolyDP(cnt, 0.01 * cv2.arcLength(cnt2, True), True)  # aproximeaza suprafata
@@ -61,34 +44,45 @@ def stopOrPark(frame,AMPARCAT=False):
             if counterRed > 1 and haveWhite is True:
                 cv2.drawContours(frame, [approx], 0, (0, 0, 0), 1)
                 cv2.imshow("ROSU", frame)
-                return Indicator.STOP
-
-    # ALBASTRU
-    ## TODO: sa cautam aici doar dupa ce facem U turn - adica sa avem o stare MergInainteDupaUturn
-    if AMPARCAT==True:
-        return None
-    contoursBlue, none = cv2.findContours(maskBlue, cv2.RETR_TREE, cv2.CHAIN_APPROX_SIMPLE)
-    for cnt in contoursBlue:
-        haveWhite = False
-        area = cv2.contourArea(cnt)
-        approx = cv2.approxPolyDP(cnt, 0.01 * cv2.arcLength(cnt, True), True)  # aproximeaza suprafata
-
-        if area > 500:
-            counterBlue = counterBlue + 1
-            for cnt2 in contoursWhite:
-                approxWhite = cv2.approxPolyDP(cnt, 0.01 * cv2.arcLength(cnt2, True), True)  # aproximeaza suprafata
-                if len(approxWhite) > 5:
-                    haveWhite = True
-            if counterBlue > 1 and haveWhite:
-                cv2.drawContours(frame, [approx], 0, (0, 0, 0), 1)
-                cv2.imshow("BLUE", frame)
                 print(area)
-                return Indicator.PARCARE
+                return True
 
+    if not AmParcat:
+        # ALBASTRU
+        lower_blue = np.array([90, 80, 50])
+        upper_blue = np.array([110, 255, 255])
+        maskBlue = cv2.inRange(hsv, lower_blue, upper_blue)
+
+        kernelBlue = np.ones((1, 1), np.uint8)
+        counterBlue = 0
+        contoursBlue, none = cv2.findContours(maskBlue, cv2.RETR_TREE, cv2.CHAIN_APPROX_SIMPLE)
+        for cnt in contoursBlue:
+            haveWhite = False
+            area = cv2.contourArea(cnt)
+            approx = cv2.approxPolyDP(cnt, 0.01 * cv2.arcLength(cnt, True), True)  # aproximeaza suprafata
+
+            if area > 550 and area < 1000:
+                counterBlue = counterBlue + 1
+                for cnt2 in contoursWhite:
+                    approxWhite = cv2.approxPolyDP(cnt, 0.01 * cv2.arcLength(cnt2, True), True)  # aproximeaza suprafata
+                    if len(approxWhite) > 5:
+                        haveWhite = True
+                if counterBlue > 1 and haveWhite:
+                    cv2.drawContours(frame, [approx], 0, (0, 0, 0), 1)
+                    cv2.imshow("BLUE", frame)
+                    print(area)
+                    AmParcat = True
+                    print(AmParcat)
+
+cap = cv2.VideoCapture('cameraE.avi')
+counter = 0
 while (cap.isOpened()):
+    counter = counter+1
     ret, frame = cap.read()
-    if ret is False:
-        break
-    img = frame
+    if counter < 1500:
+        continue
+    AmParcat = False
+
     stopOrPark(frame)
-    cv2.waitKey(1)
+    cv2.imshow("IMG", frame)
+    cv2.waitKey(0)
